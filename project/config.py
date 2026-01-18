57f555fb136a5220f9a72a84efc4b2f147626d39
mafalda-frere <mafalda.frere@agroparistech.fr>
2026-01-18 09:16:55 +0000

    clean paths

--- project/config.py
+++ project/config.py
@@ -1,6 +1,6 @@
 INPUT_FOLDER = "/teamspace/studios/this_studio/data"
-CLIP_FOLDER = "/teamspace/studios/this_studio/Clip_bis"
-CROP_FOLDER = "/teamspace/studios/this_studio/Crop"
+CLIP_FOLDER = "/teamspace/studios/this_studio/clip"
+CROP_FOLDER = "/teamspace/studios/this_studio/crop"
 
 NUM_FRAMES_PER_CLIP = 20
 FRAME_STEP = 4
