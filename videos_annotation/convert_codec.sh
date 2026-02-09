48116cfc833daf8e368e8c4398fbc7ff15cc68c4
Damien Glaizal <damien.glaizal03@gmail.com>
2026-02-09 11:29:58 +0100

    Update video file path in convert_codec.sh

--- videos_annotation/convert_codec.sh
+++ videos_annotation/convert_codec.sh
@@ -1,4 +1,4 @@
-for f in static/videos/*.mp4; do
+for f in static/videos/original/*.mp4; do
     filename=$(basename "$f")  # extract just the filename
     ffmpeg -i "$f" \
         -c:v libx264 -pix_fmt yuv420p -movflags +faststart \
